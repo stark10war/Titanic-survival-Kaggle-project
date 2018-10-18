e6318f5a43ea3eb091640ec347e4a2c20bf2c794
stark10war <shashank.tanwar32@gmail.com>
2018-10-18 19:50:10 +0530

    model building codes added and read me updated

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Titanic-survival-Kaggle-project
+
+####################################  THIS PROJECT IS UNDER PROGRESS  #########################################################
+
+
+Caution: don't laugh at my codes (or u can!) as I am a newbee to data science and unfortunately I am not from a coding background but 
+ 	 I have always been I interested in super cool Technologies, Coding, AI etc. thats why I do it for Fun.   ;)
+
+
+------------------this much work is done until now.--------------------------------------
+
+>> Extensive data cleaning is done by me like i filled the missing age values by taking the average of male ,females,child 
+   seperately and then filled them.
+
+
+>> some feautre engineering is also done. I created 2 new columns as 'Is_married' and 'IsChild', by finding 
+   the title 'Mr.' , 'Mrs.', 'Miss.', 'Master.' and created the column accordingly . 
+
+
+>> different models are build using various Machine learning techniques such as logistic regression, Decision trees, Randomforest, and Neural networks.
+
+
+
+
+
+
+
+
--- Titanic survival new.R
+++ Titanic survival new.R
@@ -33,14 +33,6 @@ Titanic[Titanic ==""]<- NA
 boxplot(Titanic$Fare)
 Titanic<-Titanic[Titanic$Fare<=100,]
 
-#Feature engieering
-Titanic$IsChild[grepl(" Master. ", Titanic$Name)]<- 'yes'
-Titanic$IsChild[is.na(Titanic$IsChild)]<- 'no'
-Titanic$Is_Married[grepl(" Mrs. ", Titanic$Name)]<- 'married'
-Titanic$Is_Married[grepl(" Miss. ", Titanic$Name)| IsChild==TRUE]<-'unmarried' 
-Titanic$Is_Married[grepl(" Mr. ", Titanic$Name)]<- 'cant_say'
-Titanic$Is_Married[is.na(Titanic$Is_Married)]<- 'cant_say'
-
 #------Filling NA in Ages with category means----------------------#
 library(dplyr)
 attach(Titanic)
@@ -65,6 +57,14 @@ summary(Titanic$Embarked)
 Titanic$Embarked[is.na(Embarked)]<- "S"
 
 
+##################Feature engieering##############################################
+Titanic$IsChild[grepl(" Master. ", Titanic$Name)]<- 'yes'
+Titanic$IsChild[is.na(Titanic$IsChild)]<- 'no'
+Titanic$Is_Married[grepl(" Mrs. ", Titanic$Name)]<- 'married'
+Titanic$Is_Married[grepl(" Miss. ", Titanic$Name)| IsChild== 'yes']<-'unmarried' 
+Titanic$Is_Married[grepl(" Mr. ", Titanic$Name)]<- 'cant_say'
+Titanic$Is_Married[is.na(Titanic$Is_Married)]<- 'cant_say'
+
 #Getting relavant features
 Titanic_new<- Titanic[,-c(1,4,9,11)]
 colSums(is.na(Titanic_new))
@@ -81,13 +81,66 @@ Titanic_new <- dummy_cols(Titanic_new,remove_first_dummy = TRUE)
 #######################################################################################
 
 #Data visualisation
+#Checking correlation
 
 library(ggplot2)
 corr_matrix<- cor(Titanic_new)
-
-
-
 corrplot::corrplot(corr_matrix,method = "number")
+
+library(randomForest)
+library(MASS)
+str(Titanic_new)
+
+Titanic_new$Survived<- as.factor(Titanic_new$Survived)
+Titanic_new$Pclass_1<- as.factor(Titanic_new$Pclass_1)
+Titanic_new$Pclass_2<- as.factor(Titanic_new$Pclass_2)
+Titanic_new$Sex_female<- as.factor(Titanic_new$Sex_female)
+Titanic_new$Embarked_C<- as.factor(Titanic_new$Embarked_C)
+Titanic_new$Embarked_Q<- as.factor(Titanic_new$Embarked_Q)
+Titanic_new$IsChild_yes<- as.factor(Titanic_new$IsChild_yes)
+Titanic_new$Is_Married_married<- as.factor(Titanic_new$Is_Married_married)
+Titanic_new$Is_Married_unmarried<- as.factor(Titanic_new$Is_Married_unmarried)
+
+
+#-------------Splitting into train and test set-------------------------------------------
+set.seed(999)
+library(caTools)
+
+spl<- sample.split(Titanic_new$Survived, 0.7)
+
+titanic_train<- Titanic_new[spl == TRUE, ]
+titanic_test<-  Titanic_new[spl == FALSE, ]
+
+###################DECISION TREE#################################################
+
+library(rpart.plot)
+set.seed(999)
+tree<-rpart(Survived~., data = titanic_train)
+prp(tree)
+
+pred_tree<-predict(tree, newdata = titanic_test, type = 'class')
+confusionMatrix(data = pred_tree,reference = titanic_test$Survived)
+
+tree$variable.importance
+attach(titani)
+##################RANDOM FOREST#################################################
+
+set.seed(999)
+fit<- randomForest(formula = Survived~.,data = titanic_train, ntree = 82, mtry = 12)
+plot(fit)
+
+which.min(fit$err.rate)
+fit$err.rate
+summary(fit)
+
+pred_fit<-predict(fit, newdata = titanic_test,type = 'class')
+confusionMatrix(pred_fit, titanic_test$Survived)
+
+
+
+
+
+
 
 
 
